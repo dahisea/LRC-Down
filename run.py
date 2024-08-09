848b3287b6760c9330a80a56788b3d65fd88cf96
Dahi <81472122+dahisea@users.noreply.github.com>
2024-08-09 11:08:21 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 import requests
 import re
 import os
-import time  # 导入time模块
+import time
 
 # 定义API URL
 api_url = "https://api.i-meto.com/meting/api?server=netease&type=playlist&id=6632256805"
 
+# 自定义请求头
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
 # 获取播放列表数据
-response = requests.get(api_url)
+response = requests.get(api_url, headers=headers)
 playlist_data = response.json()
 
 # 创建保存歌词文件的目录
@@ -23,7 +30,7 @@ for song in playlist_data:
     lyrics_url = song['lrc']
     
     # 获取歌词数据
-    lyrics_response = requests.get(lyrics_url)
+    lyrics_response = requests.get(lyrics_url, headers=headers)
     lyrics = lyrics_response.text
     
     # 定义歌词文件名
