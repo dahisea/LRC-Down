2cc7dd1bb109d7b5e4bf6e2e99cc3835b21f87ea
Dahi <81472122+dahisea@users.noreply.github.com>
2024-10-01 23:06:49 +0800

    Update run.py

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ except requests.exceptions.RequestException as e:
 # 创建保存歌词文件的目录
 os.makedirs("lyrics", exist_ok=True)
 
-# 文件名最大长度（Windows系统通常限制为260个字符）
+# 文件名最大长度
 MAX_FILENAME_LENGTH = 105
 
 # 生成安全的文件名
@@ -41,23 +41,16 @@ def safe_filename(artist, title):
         filename = f"{hash_str}.lrc"
     return f"lyrics/{filename}"
 
-
-def clean_and_format_lyrics(lyrics):
-    # 使用正则表达式删除
-    no_header_lyrics_n = re.sub(
-    r'^\[.*?\]\s*([\u4e00-\u9fa5\u3040-\u30FFa-zA-Z\s]+)\s*[:：].*', 
-    '', 
-    lyrics, 
-    flags=re.MULTILINE
-)
+    # 删除信息
+    no_header_lyrics = re.sub(r'^\[.*?]( |)*(作词|作詞|作曲|编曲|編曲|演唱|歌|音乐|词曲|词|詞|曲|制作|填词|配器|演奏|作曲者|作词者|监制|录制)( |)*(:|：).*', '', lyrics, flags=re.MULTILINE)
 
     # 保留毫秒部分的前两位
     formatted_lyrics = re.sub(
     r'(\d{2}:\d{2})\.(\d{3})',
-    lambda m: f"{m.group(1)}.{m.group(2)[:2]}",  # 保持 `.` 并仅取毫秒的前两位
+    lambda m: f"{m.group(1)}.{m.group(2)[:2]}",  # 仅取毫秒的前两位
     no_header_lyrics_n
 )
-    # 删除连续的多余空行
+    # 处理连续的多余空行
     final_lyrics = re.sub(r'\n+', '\n', formatted_lyrics)
     return final_lyrics.strip()
 
@@ -75,7 +68,7 @@ for song in playlist_data:
         logging.error(f"Error fetching lyrics for {title} by {artist}: {e}")
         continue
 
-    # 检查歌词中是否包含“纯音乐，请欣赏”
+    # 检查歌词是否纯音乐
     if "纯音乐，请欣赏" not in lyrics:
         final_lyrics = clean_and_format_lyrics(lyrics)
         filename = safe_filename(artist, title)
@@ -85,6 +78,6 @@ for song in playlist_data:
             logging.info(f"Lyrics for {title} by {artist} saved successfully.")
         except IOError as e:
             logging.error(f"Error saving lyrics for {title} by {artist}: {e}")
-        time.sleep(0.2)
+        time.sleep(1)
 
 print("歌词已成功下载并保存。")
